f6c65e946329d35d8eb55ccf04341dcb9ed286d0
Joshua Pan <joshuapan8@gmail.com>
2019-10-18 16:01:36 -0400

    Create some basic testing infrastructure

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Make sure to run `pip3 install -r requirements.txt` to install all necessary req
 
 Just run `python3 server.py` to start up the server. The server boots up on port 5000, so you can access
 the GET commands via the browser by going to, for example, `http://localhost:5000/vehicles/1000`.
+
+## Running tests
+
+Run `pytest -v` to run tests. This will automatically run tests for the Smartcar API and makes sure we
+are calling the right things to the GM API also.
---
+++ test_server.py
@@ -0,0 +1,162 @@
+import pytest
+from server import app
+from gm_api import GM_Api as GM_Api
+
+@pytest.fixture
+def client():
+  app.config['TESTING'] = True
+
+  with app.test_client() as client:
+    yield client
+
+def test_vehicle_name(client, mocker):
+  mock = mocker.patch('gm_api.GM_Api.post')
+  mock.return_value = {
+    'service': 'getVehicleInfo',
+    'status': '200',
+    'data': {
+      'vin': {
+        'type': 'String',
+        'value': '123123412412'
+      },
+      'color': {
+        'type': 'String',
+        'value': 'Metallic Silver'
+      },
+      'fourDoorSedan': {
+        'type': 'Boolean',
+        'value': 'True'
+      },
+      'twoDoorCoupe': {
+        'type': 'Boolean',
+        'value': 'False'
+      },
+      'driveTrain': {
+        'type': 'String',
+        'value': 'v8'
+      }
+    }
+  }
+
+  resp = client.get('/vehicles/1234')
+  mock.assert_called_with('/getVehicleInfoService', {'id': '1234'})
+  assert(resp.json == {'vin': '123123412412', 'color': 'Metallic Silver', 'doorCount': 4, 'driveTrain': 'v8'})
+
+def test_vehicle_doors(client, mocker):
+  mock = mocker.patch('gm_api.GM_Api.post')
+  mock.return_value = {
+    'service': 'getSecurityStatus',
+    'status': '200',
+    'data': {
+      'doors': {
+        'type': 'Array',
+        'values': [
+          {
+            'location': {
+              'type': 'String',
+              'value': 'frontLeft'
+            },
+            'locked': {
+              'type': 'Boolean',
+              'value': 'False'
+            }
+          },
+          {
+            'location': {
+              'type': 'String',
+              'value': 'frontRight'
+            },
+            'locked': {
+              'type': 'Boolean',
+              'value': 'True'
+            }
+          },
+          {
+            'location': {
+              'type': 'String',
+              'value': 'backLeft'
+            },
+            'locked': {
+              'type': 'Boolean',
+              'value': 'False'
+            }
+          },
+          {
+            'location': {
+              'type': 'String',
+              'value': 'backRight'
+            },
+            'locked': {
+              'type': 'Boolean',
+              'value': 'True'
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  resp = client.get('/vehicles/1234/doors')
+  mock.assert_called_with('/getSecurityStatusService', {'id': '1234'})
+  assert(resp.json == [
+    {'location': 'frontLeft', 'locked': False},
+    {'location': 'frontRight', 'locked': True},
+    {'location': 'backLeft', 'locked': False},
+    {'location': 'backRight', 'locked': True},
+  ])
+
+def test_vehicle_fuel(client, mocker):
+  mock = mocker.patch('gm_api.GM_Api.post')
+  mock.return_value = {
+    'service': 'getEnergy',
+    'status': '200',
+    'data': {
+      'tankLevel': {
+        'type': 'Number',
+        'value': '30.2'
+      },
+      'batteryLevel': {
+        'type': 'Null',
+        'value': 'null'
+      }
+    }
+  }
+
+  resp = client.get('/vehicles/1234/fuel')
+  mock.assert_called_with('/getEnergyService', {'id': '1234'})
+  assert(resp.json == {'percent': 30.2})
+
+def test_vehicle_battery(client, mocker):
+  mock = mocker.patch('gm_api.GM_Api.post')
+  mock.return_value = {
+    'service': 'getEnergy',
+    'status': '200',
+    'data': {
+      'tankLevel': {
+        'type': 'Number',
+        'value': '15.2'
+      },
+      'batteryLevel': {
+        'type': 'Number',
+        'value': '50.3'
+      }
+    }
+  }
+
+  resp = client.get('/vehicles/1234/battery')
+  mock.assert_called_with('/getEnergyService', {'id': '1234'})
+  assert(resp.json == {'percent': 50.3})
+
+def test_vehicle_engine(client, mocker):
+  mock = mocker.patch('gm_api.GM_Api.post')
+  mock.return_value = {
+    'service': 'actionEngine',
+    'status': '200',
+    'actionResult': {
+      'status': 'EXECUTED'
+    }
+  }
+
+  resp = client.post('/vehicles/1234/engine', json={'action': 'START'})
+  mock.assert_called_with('/actionEngineService', {'id': '1234', 'command': 'START_VEHICLE'})
+  assert(resp.json == {'status': 'success'})
